0d183b578bd329f01fb99ea15c55d9b61cf723cc
Edward CHUI <edchuihk@gmail.com>
2022-02-15 05:37:07 +0100

    Update run_experiment method in Ass2 Ex4

--- Assignment 2/Ex4.py
+++ Assignment 2/Ex4.py
@@ -10,7 +10,7 @@ MATRIX_SIZES = (1, 5, 10, 15, 20, 25, 50, 75, 100, 125, 150, 200, 250, 375, 500)
 # MATRIX_SIZES = (1, 5, 10, 25, 50, 100, 200, 375, 500, 1000, 5000, 10_000)			# For comparing with and without numexpr.
 NUM_OF_EXP = 10
 
-def init_list_matrices(size: int) -> Tuple[List[list]]:
+def init_list_matrices(size: int):
 	a = [np.random.rand(size) for i in range(size)] 
 	b = [np.random.rand(size) for i in range(size)] 
 	c = [np.random.rand(size) for i in range(size)] 
@@ -52,19 +52,21 @@ def dgemm_numpy(a, b, c, size):
 def dgemm_numexpr(a, b, c, size):
 	# Note that numexpr can only be used for element-wise operation.
 	# See: https://groups.google.com/g/numexpr/c/jDc6bRSvh6c
+	
+	# Another design that does not use np.matmul, but the performance is really poor :( .
+	#
+	# bTranspose = np.transpose(b)
+	# for i in range(size):
+	# 	for j in range(size):
+	# 		x = a[i]
+	# 		y = bTranspose[j]
+	# 		c[i, j] += evaluate("sum(x * y)")
+
 	temp = np.matmul(a, b)
 	evaluate("temp + c", out=c)
 	return c
 
-def run_experiment(matrixSize, initMatrixFunc, dgemmFunc):
-	a, b, c = initMatrixFunc(matrixSize)
-	startTime = timer()
-	dgemmFunc(a, b, c, matrixSize)
-	timeSpent = timer() - startTime
-	return timeSpent
-
-
-if __name__ == "__main__":
+def run_experiment(initMatrixFunc, dgemmFunc) -> Tuple[list]:
 	avgTimeSpents = []
 	stdTimeSpents = []
 	minTimeSpents = []
@@ -72,32 +74,42 @@ if __name__ == "__main__":
 
 	for matrixSize in MATRIX_SIZES:
 		timeSpents = []
+		for t in tqdm(range(NUM_OF_EXP), desc=f"Running {dgemmFunc.__name__} - {matrixSize}*{matrixSize} matrix", ncols=100):
+			a, b, c = initMatrixFunc(matrixSize)
+			startTime = timer()
+			dgemmFunc(a, b, c, matrixSize)
+			timeSpents.append(timer() - startTime)
 
-		for t in tqdm(range(NUM_OF_EXP), desc=f"Running\t{matrixSize}*{matrixSize}\tmatrix", ncols=100):
-			# timeSpent = run_experiment(matrixSize, init_list_matrices, dgemm_list)
-			# timeSpent = run_experiment(matrixSize, init_array_matrices, dgemm_array)
-			# timeSpent = run_experiment(matrixSize, init_numpy_matrices, dgemm_numpy)
-			timeSpent = run_experiment(matrixSize, init_numpy_matrices, dgemm_numexpr)
-
-			timeSpents.append(timeSpent)
-		
 		avgTimeSpents.append(np.average(timeSpents))
 		stdTimeSpents.append(np.std(timeSpents))
 		minTimeSpents.append(min(timeSpents))
 		maxTimeSpents.append(max(timeSpents))
+	
+	return avgTimeSpents, stdTimeSpents, minTimeSpents, maxTimeSpents
 
-	# Output result
+def output_stats(stats):
 	print("Size\t"+"\t\t".join([f"{i}*{i}" for i in MATRIX_SIZES]))	
-	print("Avg\t"+"\t".join([f"{i:.4E}" for i in avgTimeSpents]))
-	print("Std\t"+"\t".join([f"{i:.4E}" for i in stdTimeSpents]))
-	print("Min\t"+"\t".join([f"{i:.4E}" for i in minTimeSpents]))
-	print("Max\t"+"\t".join([f"{i:.4E}" for i in maxTimeSpents]))
+	print("Avg\t"+"\t".join([f"{i:.4E}" for i in stats[0]]))
+	print("Std\t"+"\t".join([f"{i:.4E}" for i in stats[1]]))
+	print("Min\t"+"\t".join([f"{i:.4E}" for i in stats[2]]))
+	print("Max\t"+"\t".join([f"{i:.4E}" for i in stats[3]]))
 
-	# Plot
-	# plt.plot(MATRIX_SIZES, avgTimeSpents, label="Python Lists")
-	# plt.plot(MATRIX_SIZES, avgTimeSpents, label="Array Module")
-	# plt.plot(MATRIX_SIZES, avgTimeSpents, label="NumPy Array")
-	plt.plot(MATRIX_SIZES, avgTimeSpents, label="NumPy Array w/ numexpr")
+if __name__ == "__main__":
+	expStats = run_experiment(init_list_matrices, dgemm_list)
+	output_stats(expStats)
+	plt.plot(MATRIX_SIZES, expStats[0], label="Python Lists")
+	
+	expStats = run_experiment(init_array_matrices, dgemm_array)
+	output_stats(expStats)
+	plt.plot(MATRIX_SIZES, expStats[0], label="Array Module")
+	
+	expStats = run_experiment(init_numpy_matrices, dgemm_numpy)
+	output_stats(expStats)
+	plt.plot(MATRIX_SIZES, expStats[0], label="NumPy Array")
+	
+	expStats = run_experiment(init_numpy_matrices, dgemm_numexpr)
+	output_stats(expStats)
+	plt.plot(MATRIX_SIZES, expStats[0], label="NumPy Array w/ numexpr")
 
 	plt.title("Execution time")
 	plt.xlabel("Matrix size N*N")

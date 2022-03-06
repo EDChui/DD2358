057a11a2fb061113f4f9fe558bdd008713a73b90
Edward CHUI <edchuihk@gmail.com>
2022-03-06 21:53:26 +0100

    Profiling on original code

--- Final Project/Original Code/raytracing.py
+++ Final Project/Original Code/raytracing.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 from functools import wraps
-
-w, h = 400, 400  # Size of the screen in pixels.
+from math import ceil
+from tqdm import tqdm
 
 # Sphere properties.
 position = np.array([0., 0., 1.])
@@ -12,12 +12,12 @@ color = np.array([0., 0., 1.])
 diffuse = 1.
 specular_c = 1.
 specular_k = 50
-    
+
 # Light position and color.
 L = np.array([5., 5., -10.])
 color_light = np.ones(3)
 ambient = .05
-    
+
 # Camera.
 O = np.array([0., 0., -1.])  # Position.
 Q = np.array([0., 0., 0.])   # Pointing to.
@@ -81,7 +81,7 @@ def trace_ray(O, D):
         ** specular_k
     return col
 
-def run():
+def run(w, h):
     img = np.zeros((h, w, 3))
     # Loop through all pixels.
     for i, x in enumerate(np.linspace(-1, 1, w)):
@@ -99,9 +99,50 @@ def run():
             img[h - j - 1, i, :] = np.clip(col, 0, 1)
     return img
 
-if __name__ == "__main__":
-    img = run()
+def get_image(w, h):
+    img = run(w, h)
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
     ax.imshow(img)
     ax.set_axis_off()
     plt.show()
+
+def test_performance():
+    num_test_per_size = 10
+    sizes = [10, 25, 50, 75, 100, 125, 150, 250, 375, 500, 750, 875, 1000, 1250, 1375, 1500]
+    averages = []
+    stds = []
+    mins =[]
+    maxs =[]
+
+    # Get the time spent on run with varying size.
+    for size in sizes:
+        times = []
+        for i in tqdm(range(num_test_per_size), desc=f"Testing {size}*{size}...", ncols=100):
+            startTime = timer()
+            run(w=size, h=size)
+            times.append(timer() - startTime)
+        averages.append(np.average(times))
+        stds.append(np.std(times))
+        mins.append(np.min(times))
+        maxs.append(np.max(times))
+
+    # Output a table about the performance.
+    for i in range(ceil(len(sizes) / 5)):
+        print("Size\t"+"\t\t".join([f"{size}" for size in sizes[i*5: (i+1)*5+1]]))
+        print("Avg\t"+"\t".join([f"{i:.4E}" for i in averages[i*5: (i+1)*5+1]]))
+        print("Std\t"+"\t".join([f"{i:.4E}" for i in stds[i*5: (i+1)*5+1]]))
+        print("Min\t"+"\t".join([f"{i:.4E}" for i in mins[i*5: (i+1)*5+1]]))
+        print("Max\t"+"\t".join([f"{i:.4E}" for i in maxs[i*5: (i+1)*5+1]]))
+        print()
+
+    # Plot the average.
+    plt.plot(sizes, averages, label=f"Original")
+    plt.title("Ray Tracing")
+    plt.xlabel("Input size")
+    plt.ylabel("Average time spent (s)")
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    # get_image(w=400, h=400)
+    test_performance()
